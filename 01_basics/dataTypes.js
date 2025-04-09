5a311c4e0e9ce0c6fc24afdf8b7615cf5769d472
Iphoneuser467 <141709218+Iphoneuser467@users.noreply.github.com>
2025-04-09 02:19:13 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,16 @@
+
+const data=["prakash","krisha","manima","prakriti"];
+let myObject={
+    name:"prabin",
+    class:12,
+    rollno:33
+}
+
+const myFunction1=function()
+{
+    console.log("hello world")
+}
+
+console.log(myFunction1());
+
+ console.log(myObject)
