88465545603a846f16536140860c796d26459f64
Iphoneuser467 <141709218+Iphoneuser467@users.noreply.github.com>
2025-04-09 06:05:10 +0000

    MathsAndNumber

---
+++ 01_basics/MathsAndNumbers.js
@@ -0,0 +1,26 @@
+
+const num=100
+
+console.log(num)
+
+const num1=String(num)
+console.log(typeof(num1))
+console.log(num1.length)
+
+const balance=new Number(100);
+
+console.log(balance.toFixed(1))
+
+
+const otherNumber=234876428736
+console.log(otherNumber.toFixed(2))
+console.log(otherNumber.toLocaleString('En-In'))
+
+const num0=45.48
+console.log(Math.abs(-3))
+console.log(Math.floor(num0));
+console.log(Math.ceil(num0));
+console.log(Math.round(num0));
+
+
+
